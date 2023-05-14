2993b33fa60239c33e85b9f72cd7d0811241152d
seawesel <jrodperalta@gmail.com>
2023-05-14 13:21:21 -0400

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Dashboard from '../components/dash/Dashboard';
+
+export default function Index() {
+  return (
+    <Container>
+      <Dashboard />
+    </Container>
+  );
+}
